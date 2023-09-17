20ab80d33a0d40d419d0beee83c1c059f0acf19b
Aphelele-Dumakude <apheleledumakude@gmail.com>
2023-09-18 00:10:29 +0200

    Refactoring - create a reusable API client

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,22 +1,21 @@
-import apiClient, { FetchResponse } from "../services/api-client";
-
-import { useQuery} from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
+
 export interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatforms = () => useQuery({
-    queryKey: ['platforms'],
-    queryFn: () =>
-        apiClient
-            .get<FetchResponse<Platform>>('/platforms/lists/parents')
-            .then(res => res.data),
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24hrs
-    initialData: { count: platforms.length, results: platforms}
-});
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,28 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
+  count: number;
+  results: T[];
 }
 
-export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'c1351a8da58a4e5a9e2f5672ecb82f17'
-    }
-})
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c1351a8da58a4e5a9e2f5672ecb82f17",
+  },
+});
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then(res => res.data);
+  }
+}
+export default APIClient;
