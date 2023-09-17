257748fe20a469f3122ae4e85d3c498b5964337f
Aphelele-Dumakude <apheleledumakude@gmail.com>
2023-09-17 23:19:19 +0200

    Refactoring - fetch games with React Query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -25,7 +25,7 @@ const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
     <>
       <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
       <List>
-        {data?.results.map((genre) => (
+        {data?.results.map((genre: Genre) => (
           <ListItem key={genre.id} paddingY="5px">
             <HStack>
               <Image
